6474d8c788467beb30fa584e3d960088977c1d11
papa magueye fam Lamp <96761672+Lampfam74@users.noreply.github.com>
2024-09-30 02:14:57 +0000

    profile integration

--- routes/web.php
+++ routes/web.php
@@ -48,7 +48,7 @@ Route::middleware(['auth'])->group(function () {
     });
     Route::middleware('auth')->group(function () {
         Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-        Route::patch('/profile', [ProfileController ::class, 'update'])->name('profile.update');
+        Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
         Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     });
 require __DIR__.'/auth.php';
